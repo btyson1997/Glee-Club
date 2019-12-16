5a8be6e310fd35c6eefea42998640c2936575046
Brandon Tyson <brandon@MacBook-Pro-2.local>
2019-12-15 20:31:02 -0500

    All but message
    Signed-off-by: Brandon Tyson <brandon@MacBook-Pro-2.local>

---
+++ Glee Club/Glee ClubTests/Glee_ClubTests.swift
@@ -0,0 +1,34 @@
+//
+//  Glee_ClubTests.swift
+//  Glee ClubTests
+//
+//  Created by Brandon Tyson on 12/15/19.
+//  Copyright © 2019 XE401. All rights reserved.
+//
+
+import XCTest
+@testable import Glee_Club
+
+class Glee_ClubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
